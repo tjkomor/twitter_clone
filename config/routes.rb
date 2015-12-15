4ff30632d305ea1c82ceb6ded1b766a8da2b01ae
Tyler Komoroske <tjkomor@clemson.edu>
2015-12-15 14:00:43 -0700

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root to: 'welcome#index'
+
+  get '/auth/twitter', as: :login
+
+  get '/auth/twitter/callback', to: 'sessions#create'
+
+  get '/logout', to: 'sessions#destroy', as: :logout
+end
