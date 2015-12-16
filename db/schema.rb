db43d1f2773b96bc1678d3f84acd83160cd0aba1
Tyler Komoroske <tjkomor@clemson.edu>
2015-12-15 20:41:37 -0700

    user can see followers -user can see friends

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151215063341) do
+ActiveRecord::Schema.define(version: 20151216032933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,9 @@ ActiveRecord::Schema.define(version: 20151215063341) do
     t.string   "oauth_token_secret"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+    t.string   "image"
+    t.integer  "friends"
+    t.integer  "followers"
   end
 
 end
