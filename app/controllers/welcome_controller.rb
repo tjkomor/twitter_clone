34d410cea518e289843e63aa6c6ad0a4b4ef0745
Tyler Komoroske <tjkomor@clemson.edu>
2015-12-16 23:51:42 -0700

    basic style

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,15 +1,8 @@
 class WelcomeController < ApplicationController
-
   def index
     if current_user
       @twitter_data = api_data(current_user)
       @user = current_user
     end
-  end
-
-  private
-
-  def api_data(current_user)
-    TwitterService.new(current_user)
   end
 end
